0fa8f00165010a317726ae99c65eb95b71767c8e
ivanpandev <64047827+ivanpandev@users.noreply.github.com>
2022-08-04 08:46:11 -0700

    Add files via upload

--- test_DI_Tools.py
+++ test_DI_Tools.py
@@ -3,6 +3,7 @@ import datetime as dt
 
 def test_DI_Tools_exists():
     DI.readme()
+    print('\n')
 
 #def test_can_find_s3_objects():
 #  start_time = dt.datetime(2022, 7, 5, 3, 4, 0, tzinfo = dt.timezone.utc)
@@ -14,6 +15,9 @@ def test_basic_DI_compare():
     ground_truth = DI.DataIntegrity(DI.log_to_object('ground_truth.log', 0, 2), name='ground_truth', sampled='N')
     returned_data = DI.DataIntegrity(DI.csv_to_object('latest.csv', 5, 6), name='tile', sampled='N')
     results = ground_truth.compare(returned_data)
-    print(f'The Match Score is: {results[0]}')
+    print('\n')
     assert results[0] > 0.99
     assert results[1] < 1.00
+    print(f'The Match Score is: {round(results[0], 4)}')
+    print(f'The Maximum Delta is: {round(results[1], 4)}')
+    print('Test Passed.')
